6165e86e427c4bcabd9ba883ab96f0627a4e52f4
Vivek Upadhayay <vivek33up@gmail.com>
2023-09-01 09:52:49 +0530

    react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [advice, setAdvice] = useState("");
+  const getAdvice = async () => {
+    const response = await axios.get("https://api.adviceslip.com/advice");
+    const advice = await response.data.slip;
+    setAdvice(advice);
+  };
+  useEffect(() => {
+    getAdvice();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body>
+      <div className="card">
+        <p>ADVICE #{advice.id}</p>
+        <h2>"{advice.advice}"</h2>
+        <img className="quote" src="pattern-divider-desktop.svg" alt="line" />
+        <img className="mob" src="pattern-divider-mobile.svg" alt="line" />
+        <button className="dice" onClick={getAdvice}>
+          {" "}
+          <img src="icon-dice.svg" alt="dice" />
+        </button>
+      </div>
+    </body>
   );
 }
 
